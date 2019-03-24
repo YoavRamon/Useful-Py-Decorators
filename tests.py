aac1765af59ec3c3b34112fdb37a40bd4ca8ecfd
YoavRamon <Yoav@Sensiya.com>
2019-03-24 13:36:01 +0200

    Added easy_debugger decorator test

--- tests.py
+++ tests.py
@@ -8,5 +8,11 @@ def this_function_takes_time():
     time.sleep(3)
 
 
+@decorators.easy_debuger
+def this_function_does_something():
+    time.sleep(1)
+
+
 if __name__ == "__main__":
     this_function_takes_time()
+    this_function_does_something()
