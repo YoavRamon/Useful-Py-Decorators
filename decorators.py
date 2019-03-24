4ce2b6634fdda54dc0f8afd22fbae32948f148c4
YoavRamon <Yoav@Sensiya.com>
2019-03-24 16:54:14 +0200

    Added print_with_color decorator, and test\example

--- decorators.py
+++ decorators.py
@@ -1,4 +1,6 @@
+import sys
 import time
+import builtins
 import functools
 
 
@@ -31,6 +33,54 @@ def run_multiple_times(times):
             result = None
             for i in range(times):
                 result = f(*args, **kwargs)
+            return result
+        return wrap
+    return decorator
+
+
+def print_with_color(color):
+    colors = {'black': '30', 'red': '31', 'green': '32', 'yellow': '33',
+              'blue': '34', 'magenta': '35', 'cyan': '36', 'white': '37'}
+    base_data = '\033[' + (colors.get(color) or '0') + 'm'
+
+    def _print(*args, **kwargs):
+        """The new-style print function from py3k."""
+        fp = kwargs.pop("file", sys.stdout)
+        if fp is None:
+            return
+
+        def write(data):
+            if not isinstance(data, str):
+                data = str(data)
+            fp.write(data)
+
+        sep = kwargs.pop("sep", None)
+        end = kwargs.pop("end", None)
+
+        if kwargs:
+            raise TypeError("invalid keyword arguments to print()")
+
+        newline = "\n"
+        space = " "
+        if sep is None:
+            sep = space
+        if end is None:
+            end = newline
+        write(base_data)
+        for i, arg in enumerate(args):
+            if i:
+                write(sep)
+            write(arg)
+        write(end)
+        write('\033[0m')  # Reset Color
+
+    def decorator(f):
+        @functools.wraps(f)
+        def wrap(*args, **kwargs):
+            old_print = builtins.print
+            builtins.print = _print
+            result = f(*args, **kwargs)
+            builtins.print = old_print
             return result
         return wrap
     return decorator
--- tests.py
+++ tests.py
@@ -8,6 +8,7 @@ def this_function_takes_time():
     time.sleep(3)
 
 
+@decorators.print_with_color('blue')
 @decorators.easy_debugger
 def this_function_does_something():
     time.sleep(1)
@@ -19,6 +20,7 @@ def this_print_words(words):
 
 
 if __name__ == "__main__":
+
     this_function_takes_time()
     this_function_does_something()
     this_print_words('Yoav Love Halav')
