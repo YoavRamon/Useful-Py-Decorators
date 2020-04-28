cdbeabe1fa75eb9ec8c864e0c5f4e15899d5b5cd
Yoav Ramon <yoav@hi.auto>
2020-04-28 17:21:14 +0300

    Add setup.py to enable release

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='useful-py-decorators',
+    version='1.0',
+    packages=find_packages(),
+    url='http://github.com/YoavRamon/Useful-Py-Decorators',
+    download_url='http://github.com/YoavRamon/Useful-Py-Decorators/releases',
+    license='',
+    author='YoavRamon',
+    author_email='ramonyoav@gmail.com',
+    description='',
+    zip_safe=False,
+    python_requires='>=3.6, <4'
+)
